d19f337b2e18bf37dfa8962ecfeeb1e01212e19f
Dianne Eramo <dianne.eramo@gmail.com>
2017-10-10 15:27:11 -0400

    Exercise 5: Refactor
    * Use map instead of a for loop
    * Use !! so we don’t have to check for undfined
    
    Learned about the !! trick to coerce return values into true or false

--- pangram/pangram.js
+++ pangram/pangram.js
@@ -8,19 +8,17 @@ const ALPHABET = ("abcdefghijklmnopqrstuvwxyz").split("");
 Pangram.prototype.isPangram = function() {
   // Without an explicit return statement, A JS function returns undefined
   // Ruby returns the value of the last line
-  for ( var i = 0; i < ALPHABET.length; i++ ) {
-    let currentLetter = ALPHABET[i]
-    if ( this.containsLetter(currentLetter) === undefined ) {
-      return false;
+  let containsAllLetters = true;
+  ALPHABET.map((currentLetter) => {
+    if ( this.containsLetter(currentLetter) === false ) {
+      containsAllLetters = false;
     }
-  }
-  return true;
+  })
+  return containsAllLetters;
 }
 
 Pangram.prototype.containsLetter = function(letter) {
-  if ( this.sentence.match(letter) ) {
-    return true;
-  }
+  return !!this.sentence.match(letter)
 }
 
 module.exports = Pangram;
